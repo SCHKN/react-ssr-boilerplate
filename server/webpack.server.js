ce5bd46237dea6477d994e0e9af0d13f10193de2
Antoine <solnichkin.antoine@gmail.com>
2018-11-14 13:00:05 +0100

    webpack optimizations

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,5 +1,9 @@
 const path = require("path");
-module.exports = {
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require("webpack-node-externals");
+
+const config = {
   // Inform webpack that we are building a bundle for node js,
   // rather than for the browser
   target: "node",
@@ -8,25 +12,11 @@ module.exports = {
   entry: "./src/index.js",
   // Tell webpack where to put the output file that is generated
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
   },
 
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }]
-          ]
-        }
-      }
-    ]
-  }
+  externals: [webpackNodeExternals()]
 };
+
+module.exports = merge(baseConfig, config);

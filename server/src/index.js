4934fbeb5a4cf06fae140887ed413faf300fbc20
Antoine <solnichkin.antoine@gmail.com>
2018-11-14 16:41:05 +0100

    redux handling

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,18 @@
 // This is the boilerplate for the renderer server part of our app.
+import 'babel-polyfill';
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // Load data into the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {

b21f8c751e49bc2dd08d89e544077f07efdbd6ba
Antoine <solnichkin.antoine@gmail.com>
2018-11-14 10:06:11 +0100

    express setup

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+// This is the boilerplate for the renderer server part of our app.
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});

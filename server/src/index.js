5578d54f97a80533d96b1146e791054e8f0f9479
Antoine <solnichkin.antoine@gmail.com>
2018-11-14 11:08:55 +0100

    smoother webpack reloading

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,10 @@ const renderToString = require("react-dom/server").renderToString;
 const Home = require("./client/components/Home").default;
 const app = express();
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");

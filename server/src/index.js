e7bc21ac45643ec1c1ab11e4374b15c1af703032
Antoine <solnichkin.antoine@gmail.com>
2018-11-14 10:24:37 +0100

    webpack config annotated

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 // This is the boilerplate for the renderer server part of our app.
 const express = require("express");
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
 const app = express();
 
 app.get("/", (req, res) => {});
